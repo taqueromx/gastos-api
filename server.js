7d36857b104dd17f3d72f2e0c1a9c2d05fb92fcf
Alejandro Longoria <alongoriae@gmail.com>
2020-01-17 13:08:26 -0600

    Delete unneeded api methods

--- server.js
+++ server.js
@@ -64,8 +64,8 @@ app.use( morgan( "dev" ) );
 // });
 
 app.get( "/api/gastosVP", ( req, res, next ) => {
-	// let vpUsuario = req.body;
-	// let vp = vpUsuario;
+	let vpUsuario = req.body;
+	let vp = vpUsuario;
 
 	VPList.get()
 		.then( vicepresidencia => {
